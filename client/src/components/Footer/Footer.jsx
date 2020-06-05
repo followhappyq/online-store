66f47f0e1b9e9f7e9acfc963e438aed39ddef7a9
Ivan <followhappyq@gmail.com>
2020-06-05 20:01:05 +0200

    added footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react"
+import {
+  InstagramOutlined,
+  FacebookOutlined,
+  LinkedinOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons"
+
+import "./footer.scss"
 
 const Footer = () => {
-  return <div></div>
+  return (
+    <div className="footer">
+      <div className="footer__title">
+        <h3>Online Store</h3>
+      </div>
+      <div className="footer__socials">
+        <InstagramOutlined className="footer__socials__item" />
+        <FacebookOutlined className="footer__socials__item" />
+        <LinkedinOutlined className="footer__socials__item" />
+        <TwitterOutlined className="footer__socials__item" />
+      </div>
+      <div className="footer__copyrights">
+        <p>Copyrights &copy; 2020</p>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
